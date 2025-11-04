ce1f3051ad866599ee3bca509396d8b5d34beac1
cristo-j <cristo.j@aluno.ifsc.edu.br>
2025-11-04 18:02:05 -0300

    Banco de dados
    Atualizado o script de criação do banco de dados e o diagrama

--- modelagem/advocacia.sql
+++ modelagem/advocacia.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `advogado`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `advogado` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) DEFAULT NULL,
   `oab` varchar(10) NOT NULL,
   `especialidade` varchar(50) DEFAULT NULL,
@@ -33,7 +33,7 @@ CREATE TABLE `advogado` (
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `oab_UNIQUE` (`oab`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,6 +42,7 @@ CREATE TABLE `advogado` (
 
 LOCK TABLES `advogado` WRITE;
 /*!40000 ALTER TABLE `advogado` DISABLE KEYS */;
+INSERT INTO `advogado` VALUES (1,'Cristo Ivanov Junior','999990','Trabalhista','2025-11-03 02:19:05','2025-11-03 02:19:05'),(2,'Vicente Borges de Camargo','1111','Tributarista','2025-11-04 00:08:39','2025-11-04 00:08:39');
 /*!40000 ALTER TABLE `advogado` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -64,7 +65,7 @@ CREATE TABLE `processo` (
   UNIQUE KEY `numero_processo_UNIQUE` (`numero_processo`),
   KEY `fk_advogado_id_processo_idx` (`id_advogado`),
   CONSTRAINT `fk_advogado_id_processo` FOREIGN KEY (`id_advogado`) REFERENCES `advogado` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -73,6 +74,7 @@ CREATE TABLE `processo` (
 
 LOCK TABLES `processo` WRITE;
 /*!40000 ALTER TABLE `processo` DISABLE KEYS */;
+INSERT INTO `processo` VALUES (1,'5004231-53.2017.4.04.7206','Ação trabalhista com pedido de horas extras','em andamento',1,'2025-11-04 15:30:10','2025-11-04 15:30:10'),(7,'5004233-53.2017.4.04.7206','Ação civl com pedido de indenização por danos morais','em andamento',1,'2025-11-04 15:38:41','2025-11-04 15:38:41');
 /*!40000 ALTER TABLE `processo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -86,13 +88,13 @@ DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE `usuario` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(60) DEFAULT NULL,
-  `email` varchar(60) NOT NULL,
-  `senha` varchar(64) NOT NULL,
+  `email` varchar(60) DEFAULT NULL,
+  `senha` varchar(64) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -101,6 +103,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,NULL,NULL,NULL,'2025-11-03 01:49:18','2025-11-03 01:49:18'),(2,NULL,NULL,NULL,'2025-11-03 01:54:00','2025-11-03 01:54:00'),(3,'Cristo','cristo@teste.com','46070d4bf934fb0d4b06d9e2c46e346944e322444900a435d7d9a95e6d7435f5','2025-11-03 01:56:35','2025-11-03 01:56:35');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -113,7 +116,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-11-03  1:43:37
-
-
-
+-- Dump completed on 2025-11-04 17:55:36
